ab1bd27f7ee5792c9e82b50815bfd21e1f70f3f0
JAYAMSOLUTIONS\BrahmaReddy <brahma.reddy@jayamsolutions.com>
2022-09-03 19:11:03 +0530

    added login text at line no11

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -9,6 +9,7 @@ const Login = () => {
     const dispatch = useDispatch();
   return (
     <div><section className="vh-100" style={{backgroundColor:'white'}} >
+      hello
     <div className="container py-5 h-100">
       <div className="row d-flex justify-content-center align-items-center h-100">
         <div className="col col-xl-10">
